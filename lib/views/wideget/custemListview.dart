c183a3ee45df8ea969abc4f2501f48cd068e5eb6
Gamal Osama <gamal8686@gmail.com>
2025-08-31 10:44:45 +0300

    create CustemListview in Bady

---
+++ lib/views/wideget/custemListview.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustemListview extends StatelessWidget {
+  const CustemListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, index) => Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            color: Color(0xffFFcc80),
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: Text(
+                    'Flutter',
+                    style: TextStyle(
+                      fontFamily: 'Schyler',
+                      color: Colors.black,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Biuld your carrer whith Gamal Osama',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.black.withAlpha(150),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    color: Colors.black,
+                    onPressed: () {},
+                    icon: Icon(Icons.delete, size: 30),
+                  ),
+                ),
+                Text(
+                  '12/12/2202',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/wideget/notesviewBade.dart
+++ lib/views/wideget/notesviewBade.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/wideget/custemABBar.dart';
+import 'package:note_app/views/wideget/custemListview.dart';
 
 class notesviewBade extends StatelessWidget {
   const notesviewBade({super.key});
@@ -8,7 +9,14 @@ class notesviewBade extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(children: [SizedBox(height: 50), custemABBar()]),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          custemABBar(),
+          SizedBox(height: 50),
+          CustemListview(),
+        ],
+      ),
     );
   }
 }
