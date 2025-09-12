7fe0b4af760b262bffc5839fc55d453ec0d7cf11
Gamal Osama <gamal8686@gmail.com>
2025-09-13 01:30:56 +0300

    Register adaptor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/model/note_model.dart';
 import 'package:note_app/views/nots_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('name');
+  Hive.registerAdapter(noteModelAdapter());
   runApp(const noteApp());
 }
 
--- lib/views/wideget/cubit/pageCuibit.dart
+++ lib/views/wideget/cubit/pageCuibit.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/model/note_model.dart';
 import 'package:note_app/views/wideget/cubit/pagestate.dart';
 
 class pacegcuibt extends Cubit<Pagestate> {
   pacegcuibt() : super(Pagestate());
+
+  addnot(noteModel) {}
 }
